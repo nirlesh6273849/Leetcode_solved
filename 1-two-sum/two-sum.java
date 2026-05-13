ef49edf9b4a81d48aa457aca0dcef56b49c4bf1b
Nirlesh <122976836+nirlesh6273849@users.noreply.github.com>
2026-05-13 22:36:38 +0530

    Runtime 0 ms Beats 100% Memory =0.58 MB Beats 99.99%

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(!h.containsKey(nums[i])){
                 h.put(target-nums[i],i);
-            }
+            } 
             else return new int[] {i,h.get(nums[i])};
         }
         return new int[]{};
