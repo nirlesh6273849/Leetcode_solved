95b2e6332d779d343104eddc66f82826edcadacd
Nirlesh <122976836+nirlesh6273849@users.noreply.github.com>
2026-05-17 11:50:15 +0530

    Runtime 1 ms Beats 92.42% Memory 53.08 MB Beats 24.27%

--- Contains-Duplicate.java
+++ Contains-Duplicate.java
@@ -1,7 +1,7 @@
 1class Solution {
 2    public boolean containsDuplicate(int[] nums) {
 3        Set<Integer> seen = new HashSet<>();
-4        for (int i =0; i< nums.length; i++){
+4        for (int i =0; i< nums.length; i++){ 
 5            if (!seen.add(nums[i])){
 6                return true;
 7            }
